eeff6e3d6eab2d5f3f904075bef05b8c674e3cea
DNwin <ndennis@gmail.com>
2015-05-20 16:07:33 -0700

    Initial Commit

---
+++ Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by Dennis Nguyen on 5/20/15.
+//  Copyright (c) 2015 dnwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
